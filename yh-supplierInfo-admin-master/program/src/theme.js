5bc23719c5525721acb13ac80409cfa5581871f9
isYaoHaoe <caocaojianke@gmail.com>
2019-01-23 01:21:48 +0800

    init

--- yh-supplierInfo-admin-master/program/src/theme.js
+++
@@ -1,6 +0,0 @@
-// https://github.com/ant-design/ant-design/blob/master/components/style/themes/default.less
-module.exports = {
-  'primary-color': '#0275d8',
-  'card-actions-background': '#f5f8fa',
-  'layout-sider-background': '#ffffff',
-};
